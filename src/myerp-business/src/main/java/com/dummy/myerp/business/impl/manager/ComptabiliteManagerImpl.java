2f609c0f37316e1611673929ff2985abb7581473
Joseph Herradi <joseph.herradi@gmail.com>
2019-09-22 13:32:54 +0100

    implem RG5 ComptabiliteManagerImpl

--- src/myerp-business/src/main/java/com/dummy/myerp/business/impl/manager/ComptabiliteManagerImpl.java
+++ src/myerp-business/src/main/java/com/dummy/myerp/business/impl/manager/ComptabiliteManagerImpl.java
@@ -1,6 +1,7 @@
 package com.dummy.myerp.business.impl.manager;
 
 import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.Set;
 import javax.validation.ConstraintViolation;
@@ -132,8 +133,19 @@ public class ComptabiliteManagerImpl extends AbstractBusinessManager implements 
                 "L'écriture comptable doit avoir au moins deux lignes : une ligne au débit et une ligne au crédit.");
         }
 
-        // TODO ===== RG_Compta_5 : Format et contenu de la référence
+        // DONE ===== RG_Compta_5 : Format et contenu de la référence
         // vérifier que l'année dans la référence correspond bien à la date de l'écriture, idem pour le code journal...
+        String dateEcriture= new SimpleDateFormat("yyyy").format(pEcritureComptable.getDate());
+       
+        if (pEcritureComptable.getReference().substring(3, 7).equals(dateEcriture))
+            throw new FunctionalException(
+                    "La date d'écriture ne correspond pas à la date de référence");
+        
+        if (pEcritureComptable.getReference().substring(0, 1).equals(pEcritureComptable.getJournal().getCode()))
+            throw new FunctionalException(
+                    "Le code journal dans la référence est erroné, il ne correspond pas au code du bon journal");
+    }
+        
     }
 
 
